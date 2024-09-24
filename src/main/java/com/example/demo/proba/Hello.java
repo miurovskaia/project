5c8dd05d740b0cf56749c5164848d1100f457b8c
Marina Yurovskaya <miurovskaia@neoflex.ru>
2024-09-24 15:06:14 +0300

    first commit

---
+++ src/main/java/com/example/demo/proba/Hello.java
@@ -0,0 +1,10 @@
+package com.example.demo.proba;
+
+import org.springframework.stereotype.Component;
+
+public class Hello
+{
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
