ebe238503c4f87fe7ac0fc201721a55791701bfd
Marina Yurovskaya <miurovskaia@neoflex.ru>
2025-12-26 22:39:38 +0300

    add auth

--- demo/src/main/java/com/example/demo/StudyprojectApplication.java
+++ demo/src/main/java/com/example/demo/StudyprojectApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.auth.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class StudyprojectApplication {
@@ -10,4 +14,16 @@ public class StudyprojectApplication {
 		SpringApplication.run(StudyprojectApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public FilterRegistrationBean<AuthFilter> authFilter() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new AuthFilter(restTemplate()));
+		registrationBean.addUrlPatterns("/client/*");
+		return registrationBean;
+	}
 }
